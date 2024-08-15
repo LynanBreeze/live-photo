51ec0cc0bbc309c0b2457c1fb718a818bd104949
Lynan <24271362+LynanBreeze@users.noreply.github.com>
2024-08-15 22:03:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Live-Photo
+
+A simple live-photo React component.
+
+## Preview
+
+
+
+## Options
+
+| Key                     | Type                                          |
+| ------------------------ | --------------------------------------------- |
+| photoSrc               | string, required |
+| videoSrc  | string, required |
+| loop               |  null | 1 |
+| muted               |  null | 1 |
+| useApple               |  null | 1 |
+
+
+## Run
+
+```shell
+pnpm install && pnpm run dev
+```
+
+## Build
---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import { useState, useEffect, useRef } from "react";
+import * as LivePhotosKit from "livephotoskit";
+import queryString from "query-string";
+import "./App.css";
+
+const LivePhotosKitReact = ({ className, photoSrc, videoSrc }) => {
+  let nodeRef = useRef(null);
+
+  useEffect(() => {
+    const player = LivePhotosKit.Player(nodeRef.current);
+    player.photoSrc = photoSrc;
+    player.videoSrc = videoSrc;
+  }, []);
+
+  return <div ref={nodeRef} className={className}></div>;
+};
+
+const LivePhoto = (props) => {
+  const { photoSrc, videoSrc, muted, loop, useApple } = props;
+  const [imgReady, setImgReady] = useState(false);
+  const [videoPlaying, setVideoPlaying] = useState(false);
+  const [videoRunning, setVideoRunning] = useState(false);
+  const [videoReady, setVideoReady] = useState(false);
+  const videoRef = useRef(null);
+
+  const playVideo = () => {
+    if (videoRunning) {
+      videoRef.current.pause();
+    } else {
+      setVideoPlaying(true);
+      videoRef.current.play();
+    }
+  };
+
+  const openPreview = (url) => {
+    window.top.postMessage(url, window.location.origin);
+  };
+
+  const onClick = (e) => {
+    if (!/trigger/.test(e.target.className) && !videoPlaying) {
+      openPreview(photoSrc);
+    }
+  };
+
+  const onImageLoad = () => {
+    setImgReady(true);
+    if (
+      /iphone/i.test(navigator.userAgent) &&
+      /micromessenger/i.test(navigator.userAgent)
+    ) {
+      setTimeout(() => {
+        setVideoReady(true);
+      }, 500);
+    }
+  };
+
+  return (
+    <div className='live-photo' onClick={onClick}>
+      {useApple ? (
+        <LivePhotosKitReact
+          className='live-img'
+          photoSrc={photoSrc}
+          videoSrc={videoSrc}
+        />
+      ) : (
+        <>
+          <div
+            className='live-trigger'
+            onClick={playVideo}
+            style={{ opacity: Number(videoReady) }}
+          >
+            <div
+              className='trigger-icon'
+              style={{
+                animationPlayState: videoRunning ? "running" : "paused",
+              }}
+            ></div>
+            <span className='trigger-text'>LIVE</span>
+          </div>
+          <img
+            className='live-img'
+            src={photoSrc}
+            onLoad={onImageLoad}
+            style={{ opacity: Number(imgReady) }}
+          />
+          <video
+            playsInline
+            webkit-playsinline
+            loop={loop}
+            muted={muted}
+            ref={videoRef}
+            className='live-video'
+            src={videoSrc}
+            style={{ opacity: Number(videoPlaying) }}
+            onCanPlay={() => setVideoReady(true)}
+            onLoadedMetadata={() => setVideoReady(true)}
+            onPlaying={() => setVideoRunning(true)}
+            onPause={() => setVideoRunning(false)}
+            onEnded={() => setVideoPlaying(false)}
+          ></video>
+        </>
+      )}
+    </div>
+  );
+};
+
+function App() {
+  const [photoSrc, setPhotoSrc] = useState("");
+  const [videoSrc, setVideoSrc] = useState("");
+  const [useApple, setUseApple] = useState(false);
+  const [muted, setMuted] = useState(false);
+  const [loop, setLoop] = useState(false);
+
+  useEffect(() => {
+    const parsed = queryString.parse(location.search);
+    const { picUrl, videoUrl, photoSrc, videoSrc, muted, loop, useApple } =
+      parsed;
+    setPhotoSrc(picUrl || photoSrc);
+    setVideoSrc(videoUrl || videoSrc);
+    setMuted(!!muted);
+    setUseApple(!!useApple);
+    setLoop(!!loop);
+  }, []);
+
+  return (
+    <>
+      {!!photoSrc && !!videoSrc ? (
+        <LivePhoto
+          photoSrc={photoSrc}
+          videoSrc={videoSrc}
+          muted={muted}
+          useApple={useApple}
+          loop={loop}
+        />
+      ) : null}
+    </>
+  );
+}
+
+export default App;
